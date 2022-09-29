857fe2493e272829ebd5c1eb15c9e9d022a4c741
sinat <sinatnang1998@gmail.com>
2022-09-29 15:25:19 +0700

    test new calculatior

--- App.js
+++ App.js
@@ -1,110 +1,45 @@
-import React, { Component } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import Button from "./components/Button";
-import Row from "./components/Row";
-import calculator, { initialState } from "./components/util/calculator";
+// import React, { Component } from "react";
+import { useState } from "react";
+import { StatusBar } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Switch } from "react-native";
+// import Button from "./components/Button";
+// import Row from "./components/Row";
+// import calculator, { initialState } from "./components/util/calculator";
+import {myColors} from "./src/styles/Colors";
+import { ThemeContext } from "./src/context/ThemeContext";
+import Button from "./src/commponents/Button";
+import MyKeyboard from "./src/commponents/MyKeyboard";
 
 // create class component of App
-export default class App extends Component {
-  state = initialState;
+export default function App() {
+ 
+  const [theme,setTheme] = useState('light');
 
-  // handle tap method
-  HandleTap = (type, value) => {
-    this.setState((state) => calculator(type, value, state));
-  };
+  // render() {
 
-  // render method
-  render() {
     return (
-      <View style={styles.container}>
-        {/* Status bae here */}
-        <SafeAreaView>
-          <Text style={styles.value}>
-            {parseFloat(this.state.currentValue).toLocaleString()}
-          </Text>
-
-          {/* Do create componentRow */}
-          <Row>
-            <Button
-              text="C"
-              theme="secondary"
-              onPress={() => this.HandleTap("clear")}
-            />
-
-            <Button
-              text="+/-"
-              theme="secondary"
-              onPress={() => this.HandleTap("posneg")}
-            />
-
-            <Button
-              text="%"
-              theme="secondary"
-              onPress={() => this.HandleTap("percentage")}
-            />
-
-            <Button
-              text="/"
-              theme="accent"
-              onPress={() => this.HandleTap("operator", "/")}
-            />
-          </Row>
-
-          {/* Number */}
-          <Row>
-            <Button text="7" onPress={() => this.HandleTap("number", 7)} />
-            <Button text="8" onPress={() => this.HandleTap("number", 8)} />
-            <Button text="9" onPress={() => this.HandleTap("number", 9)} />
-            <Button
-              text="X"
-              theme="accent"
-              onPress={() => this.HandleTap("operator", "*")}
-            />
-          </Row>
-
-          <Row>
-            <Button text="5" onPress={() => this.HandleTap("number", 5)} />
-            <Button text="6" onPress={() => this.HandleTap("number", 6)} />
-            <Button text="7" onPress={() => this.HandleTap("number", 7)} />
-            <Button
-              text="-"
-              theme="accent"
-              onPress={() => this.HandleTap("operator", "-")}
-            />
-          </Row>
-
-          <Row>
-            <Button text="1" onPress={() => this.HandleTap("number", 1)} />
-            <Button text="2" onPress={() => this.HandleTap("number", 2)} />
-            <Button text="3" onPress={() => this.HandleTap("number", 3)} />
-            <Button
-              text="+"
-              theme="accent"
-              onPress={() => this.HandleTap("operator", "+")}
-            />
-          </Row>
-
-          <Row>
-            <Button text="0" onPress={() => this.HandleTap("number", 0)} />
-            <Button text="." onPress={() => this.HandleTap("number", ".")} />
-            <Button
-              text="="
-              theme="primary"
-              onPress={() => this.HandleTap("equal", "=")}
-            />
-          </Row>
-        </SafeAreaView>
-      </View>
+      <ThemeContext.Provider value={theme}>
+        <View style={theme === 'light'? styles.container: [styles.container, {backgroundColor:'#000'}]}>
+            <StatusBar style="auto"/>
+            <Switch
+              value={theme === 'light'}
+              onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+              />
+              <MyKeyboard/>
+        </View>
+      </ThemeContext.Provider>
+      
     );
-  }
+  // }
 }
 
 // create styles of app
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#202020",
-    justifyContent: "flex-end",
+    backgroundColor: myColors.light,
+    alignItems:'center',
+    justifyContent: "flex-start",
   },
   value: {
     color: "#fff",
